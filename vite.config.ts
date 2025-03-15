aafd3704fde8016a4989e2de48fb94e29a264c3c
gautam071 <gautamr.slm@gmail.com>
2025-03-15 17:42:23 +0530

    Updated the deployment code

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,9 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "gautam071.github.io", // 👈 Replace with your actual GitHub repo name
   plugins: [react()],
   optimizeDeps: {
-    exclude: ['lucide-react'],
+    exclude: ["lucide-react"],
   },
 });
